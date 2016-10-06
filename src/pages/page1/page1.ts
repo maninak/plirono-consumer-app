37b0af4bd75f84ff5cb1b5f4fcc8eb6f7fb3c481
Kostis Maninakis <kmaninak@gmail.com>
2016-10-06 18:45:55 +0300

    Implements DeeplingDataProvider. Parses launch url and holds data globally

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DeeplinkDataProvider } from '../../providers/deeplink-data.provider';
+
 
 @Component({
   selector:     'page-page1',
-  templateUrl:  'page1.html'
+  templateUrl:  'page1.html',
+  providers:    [DeeplinkDataProvider]
 })
 export class Page1 {
-  amount:       number = 0;
-  description:  string = '';
-
-  constructor(public navCtrl: NavController) {
-    console.log('LaunchUrl = '+localStorage.getItem('launchUrl'));
+  amount: number;
+  description: string;
+   
+  constructor(
+      private navCtrl: NavController
+      , private deeplinkDataProvider: DeeplinkDataProvider 
+    ) {
+      this.deeplinkDataProvider.init()
+      setTimeout( () => {
+        this.amount = this.deeplinkDataProvider.params.amount;
+        console.log('LaunchUrl = '+this.deeplinkDataProvider.launchUrl); // TODO delete
+        console.log(this.deeplinkDataProvider.params); // TODO delete
+    }, 1000);
   }
-
+  
 }
