cb75f22b88652eb64f8eea9d8f0303ac132695e5
Kostis Maninakis <kmaninak@gmail.com>
2016-10-06 12:28:35 +0300

    Implements conditional appearance of  Description, minor formatting changes

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-page1',
-  templateUrl: 'page1.html'
+  selector:     'page-page1',
+  templateUrl:  'page1.html'
 })
 export class Page1 {
-  amount: number = 0;
-
+  amount:       number = 0;
+  description:  string = '';
 
   constructor(public navCtrl: NavController) {
     
