e4952ca88a2063e74a8e8ce1dd10b735625435aa
Kostis Maninakis <kmaninak@gmail.com>
2016-10-08 03:08:32 +0300

    Reimplements launchUrl propagation entirely, discarding workarounds and using events
    Solves bug where deeplinking into a standard launched but hibernated app didn't update its state to deeplink mode. + all other inconsistent state scenarios

--- src/providers/deeplink-data.provider.ts
+++ src/providers/deeplink-data.provider.ts
@@ -1,34 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 
 
 @Injectable()
 export class DeeplinkDataProvider {
-  launchUrl:string;
-  url:string;
-  params:any  = {};
-
-
-  constructor(){}
-
-  init():boolean {
-    if (this.launchUrl = localStorage.getItem('launchUrl')) {
-      localStorage.removeItem('launchUrl');
-    }
-    else {
-      console.warn("DeeplinkDataProvider was not initilised. Application wasn't launched via deeplink procedure.");
-      return false;
-    }
-    let splitToComponentsRegex  = /(.*:\/\/)(.*)\?(.*)/;
-    let urlScheme               = this.launchUrl.replace(splitToComponentsRegex, '$1'); 
-    let unparsedParams:string; 
-    if (urlScheme === 'tinaba://'){ // TODO load scheme from a .env configuration file
-      this.url        = this.launchUrl.replace(splitToComponentsRegex, '$2');
-      unparsedParams  = this.launchUrl.replace(splitToComponentsRegex, '$3');
-      this.params     = this.parseUrlParams(unparsedParams);
-    }
-    return true;
+  private _launchUrl$ : Subject<string>;
+  private _url$       : Subject<string>;
+  private _params$    : Subject<any>;
+  private dataStore: {
+    launchUrl         : string
+    , url             : string
+    , params          : any
   }
+
+  get launchUrl$()    { return this._launchUrl$.asObservable(); }
+  get url$()          { return this._url$.asObservable();       }
+  get params$()       { return this._params$.asObservable();    }
+
+
+  constructor(){
+    this.dataStore    = { launchUrl: '', url: '', params: {} };
+    this._launchUrl$  = <Subject<string>> new Subject();
+    this._url$        = <Subject<string>> new Subject();
+    this._params$     = <Subject<any>>    new Subject();
+
+    document.addEventListener('deeplink', (event:any) => {
+      this.dataStore.launchUrl = event.detail.launchUrl;
+      this._launchUrl$.next(this.dataStore.launchUrl);
+      this.init();
+    });
+  }
+
+
+  private init() {
+    let unparsedParams:string;
+    let splitToComponentsRegex  = /(.*:\/\/)(.*)\?(.*)/;
+
+    this.dataStore.url          = this.dataStore.launchUrl.replace(splitToComponentsRegex, '$2');
+    unparsedParams              = this.dataStore.launchUrl.replace(splitToComponentsRegex, '$3');
+    this.dataStore.params       = this.parseUrlParams(unparsedParams);
+    
+    this._url$.next(this.dataStore.url);
+    this._params$.next(this.dataStore.params);
+  }
+
 
   private parseUrlParams(unparsedParams:string) {
     var parsedParams = {};
@@ -38,5 +55,6 @@ export class DeeplinkDataProvider {
     });
     return parsedParams;
   }
+
 
 }
